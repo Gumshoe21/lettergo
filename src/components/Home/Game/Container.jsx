a06a35a2b15d0e2dfd86592efc4e1f8d3c5b7137
Matthew Smilansky <msmilansky@gmail.com>
2022-11-05 12:53:22 -0700

    fix: Made game container fit mobile screens better.

--- src/components/Home/Game/Container.jsx
+++ src/components/Home/Game/Container.jsx
@@ -1,7 +1,7 @@
 const Container = ({ children }) => {
   return (
-    <div className="max-w-xl h-[calc(100vh-73.98px)] mx-auto flex justify-center items-center">
-      <div className="flex flex-col content-center rounded-xl align-center bg-[rgb(255,255,255,0.02)]">
+    <div className="h-screen mx-auto ">
+      <div className="flex flex-col flex-1 items-stretch rounded-xl align-center bg-[rgb(255,255,255,0.02)]">
         {children}
       </div>
     </div>
